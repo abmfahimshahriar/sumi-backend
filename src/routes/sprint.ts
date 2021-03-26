d2e3318218ee999a87f3269358bf50ad3c18cae7
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-03-26 17:45:27 +0600

    delete sprint api modification

--- src/routes/sprint.ts
+++ src/routes/sprint.ts
@@ -12,5 +12,5 @@ const router = express.Router();
 router.post("/createSprint", authCheck, createSprint);
 router.get("/getSprints/:projectId", authCheck, getSprints);
 router.put("/updateSprint", authCheck, updateSprint);
-router.delete("/deleteSprint", authCheck, deleteSprint);
+router.post("/deleteSprint", authCheck, deleteSprint);
 export default router;
